839124e56a35df2b1ea4deefa86767e6f7c7450b
Alif <alaminalifaa7@gmail.com>
2021-08-06 01:16:07 +0600

    form practice vue project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-form></the-form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheForm from './components/TheForm.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    TheForm
+  }  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
 }
 </style>
